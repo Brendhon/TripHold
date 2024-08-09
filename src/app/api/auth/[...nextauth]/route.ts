238ca191a143c00231f05222b6e2690a002a17cb
Brendhon <brendhon.e.c.m@gmail.com>
2024-08-09 14:39:38 -0300

    fix: NextAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   // Configure one or more authentication providers
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
