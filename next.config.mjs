4a77d59ac886eb7766bf4af4ef1cd215fb468872
Brendhon <brendhon.e.c.m@gmail.com>
2024-08-11 18:56:13 -0300

    feat: add PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import withPWA from 'next-pwa';
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,      // Enable React strict mode for improved error handling
+  swcMinify: true,            // Enable SWC minification for improved performance
   images: {
     remotePatterns: [
       {
@@ -15,4 +18,12 @@ const nextConfig = {
   },
 };
 
-export default withNextIntl(nextConfig);
+// PWA configuration
+const withPWAConfig = {
+  dest: "public",         // destination directory for the PWA files
+  disable: process.env.NODE_ENV !== "production",        // disable PWA in development
+  register: true,         // register the PWA service worker
+  skipWaiting: true,      // skip waiting for service worker activation
+};
+
+export default withPWA(withPWAConfig)(withNextIntl(nextConfig));
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,22 @@ import "@styles/globals.css";
 export const metadata: Metadata = {
   title: "TripHold",
   description: "TripHold - Your trip planner",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    {
+      name: "Brendhon Moreira",
+      url: "https://www.linkedin.com/in/brendhon-moreira/",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "ios/128.png" },
+    { rel: "icon", url: "ios/128.png" },
+  ],
 };
 
 export default function RootLayout({
