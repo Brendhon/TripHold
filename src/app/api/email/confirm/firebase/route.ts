9ebc8d27da84c14869a906b7ff594562b6cd6030
Brendhon <brendhon.e.c.m@gmail.com>
2024-09-20 17:46:03 -0300

    fix: EmailConfirmation

--- src/app/api/email/confirm/firebase/route.ts
+++ src/app/api/email/confirm/firebase/route.ts
@@ -1,25 +1,9 @@
-import { DEFAULT_HOST } from '@utils/common';
 import { handleEmailError } from '@utils/email';
 import admin from 'lib/firebase/admin-config';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   try {
-    // Get origin from the request
-    const requestOrigin = req.nextUrl.origin;
-
-    console.log('nextUrl', req.nextUrl);
-
-    console.log('req', req);
-
-    console.log('DEFAULT_HOST', DEFAULT_HOST);
-
-    console.log('process.env.NODE_ENV', process.env.NODE_ENV);
-
-    // Check if if allowed origin 
-    if (process.env.NODE_ENV === 'production' && !DEFAULT_HOST.includes(requestOrigin))
-      throw { error: 'Invalid origin', status: 400 };
-
     // Get uid from the req body
     const { uid } = await req.json()
 
