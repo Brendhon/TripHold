4aacef04360cd6ac9725f94c1a2520bd303c8ebc
Brendhon <brendhon.e.c.m@gmail.com>
2024-09-25 10:56:21 -0300

    fix: TripDetails

--- src/app/[locale]/(private)/trip/[id]/page.tsx
+++ src/app/[locale]/(private)/trip/[id]/page.tsx
@@ -1,25 +1,20 @@
 "use client";
 
-import { Trip } from "@app/models";
 import { Tab, Tabs } from "@nextui-org/react";
 import { useTrip } from "context/TripContext";
 import { useTranslations } from "next-intl";
-import { Key, useEffect, useState } from "react";
+import { Key, useState } from "react";
 
 type TabsOptions = 'schedule' | 'settings';
 
 export default function TripDetails() {
   const [selected, setSelected] = useState<TabsOptions>("schedule");
-  const [localTrip, setLocalTrip] = useState<Trip | null>(null);
 
-  // Flag context
+  // Trip context
   const { trip } = useTrip();
 
   // Translations
   const tPage = useTranslations("TripDetails");
-
-  // Use effect to set local trip
-  useEffect(() => setLocalTrip(trip), [trip]);
 
   // Render home page
   return (
@@ -32,11 +27,11 @@ export default function TripDetails() {
       onSelectionChange={(key: Key) => setSelected(key as TabsOptions)}
     >
       <Tab key="schedule" title={tPage('title.schedule')} className="py-4 px-7 md:px-16">
-        <span>Schedule {localTrip?.id}</span>
+        <span>Schedule {trip?.id}</span>
       </Tab>
 
       <Tab key="settings" title={tPage('title.settings')} className="py-4 px-7 md:px-16">
-        <span>Settings {localTrip?.id}</span>
+        <span>Settings {trip?.id}</span>
       </Tab>
 
     </Tabs>
